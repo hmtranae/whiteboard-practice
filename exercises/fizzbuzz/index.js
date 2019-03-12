6ce0a978743ce71f390e77ee7e79db68431e2888
Hieu Tran <hmtran.ae@gmail.com>
2019-03-12 15:19:39 -0400

    solution to fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,19 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let num = 1; num <= n; num++) {
+        if (num % 5 === 0 && num % 3 === 0) {
+            console.log('fizzbuzz')
+        } else if (num % 3 === 0) {
+            console.log('fizz')
+        } else if (num % 5 === 0) {
+            console.log('buzz')
+        } else {
+            console.log(num)
+        }
+    }
+        // num % 5 === 0 && num % 3 === 0 ? console.log('fizzbuzz') : num % 3 === 0 ? console.log('fizz') : num % 5 === 0 ? console.log('buzz') : console.log(num)
+}
 
 module.exports = fizzBuzz;

48f53acb9a6f198c668354d0420356f51d7e2535
Hieu Tran <hmtran.ae@gmail.com>
2019-03-13 12:32:24 -0400

    solution to chunk array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,46 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// Solution 1
+// 1. Create empty array to hold chunks called 'chunked'
+// 2. For each element in the 'unchunked' array, 
+// retrieve the last element in chunked
+//      if last element does not exist or if its length is equal to chunk size
+//      push new chunk into 'chunked' with current element
+// else add current element into chunk
+
+// function chunk(array, size) {
+//     const chunked = [];
+
+//     for (let element of array) {
+//         const last = chunked[chunked.length - 1];
+//         if (!last || last.length === size) {
+//             chunked.push([element])
+//         } else {
+//             last.push(element);
+//         }
+//     }
+    
+//     return chunked;
+// }
+
+
+// Solution 2: use slice method
+// slice(startIndex, endIndex) and returns a copy everything up to endIndex
+// 1. Create empty 'chunked' array
+// 2. Create 'index' start at 0
+// 3. While index is less than array.length
+//      Push slice of length 'size' from 'array' into 'chunked'
+//      Add 'size' to 'index'
+function chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+
+    return chunked;
+}
 
 module.exports = chunk;

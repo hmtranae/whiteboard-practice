654b9ca5c26bd6b78c664a177031f71b0e27bb85
Hieu Tran <hmtran.ae@gmail.com>
2019-03-14 13:56:01 -0400

    memoized solution to fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,33 +9,55 @@
 //   fib(4) === 3
 
 // Solution 1: Iterative
-function fib(n) {
-    const result = [0, 1];
+// function fib(n) {
+//     const result = [0, 1];
 
-    for (let i = 2; i <= n; i++) {
-        const a = result[i - 1];
-        const b = result[i - 2];
-        result.push(a + b);
-    }
-    return result[n];
-}
+//     for (let i = 2; i <= n; i++) {
+//         const a = result[i - 1];
+//         const b = result[i - 2];
+//         result.push(a + b);
+//     }
+//     return result[n];
+// }
 
 // Solution 2: Recursive
-// function fib(n) {
-//     // if (n === 0) {
-//     //     return 0;
-//     // }
+// Without memoization is exponential runtime
+function fib(n) {
+    // if (n === 0) {
+    //     return 0;
+    // }
 
-//     // if (n === 1 || n === 2) {
-//     //     return 1;
-//     // }
+    // if (n === 1 || n === 2) {
+    //     return 1;
+    // }
 
-//     // change two if clauses above because only need first 2 elements
-//     if (n < 2) {
-//         return n;
-//     }
+    // change two if clauses above because only need first 2 elements
+    if (n < 2) {
+        return n;
+    }
 
-//     return fib(n - 1) + fib(n -2);
-// }
+    return fib(n - 1) + fib(n -2);
+}
+
+function memoize(fn) {
+    // records values that we already saw 
+    const cache = {};
+    // I don't know many arguments I'm going to receive but put all
+    // of them into an array called args
+    return function(...args) {
+        // if we have that value in cache, return that value
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        // calculate that value based on function passed in and cache
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    };
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
---
+++ exercises/fib/tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+const memoizedAdd = () => {
+    let cache = {};
+    return (n) => {
+        if (n in cache) {
+            console.log('Fetching from cache');
+            return cache[n];
+        } else {
+            console.log('Calculating result');
+            let result = n + 10;
+            cache[n] = result;
+            return result;
+        }
+    }
+}
+
+const newAdd = memoizedAdd();
+
+console.log(newAdd(9));
+console.log(newAdd(9));

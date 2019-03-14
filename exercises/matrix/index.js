b221243a26a3d2da70dbcc5e437d493e90db5c44
Hieu Tran <hmtran.ae@gmail.com>
2019-03-13 21:34:51 -0400

    trying spiral matrix on my own

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -16,46 +16,50 @@
 //     [10,  9,  8, 7]]
 
 // Returns an array of array of row values
+// function matrix(n) {
+//     const results = [];
+
+//     for (let i = 0; i < n; i++) {
+//         results.push([]);
+//     }
+
+//     let count = 1;
+//     let startColumn = 0;
+//     let startRow = 0;
+//     let endColumn = n - 1;
+//     let endRow = n - 1;
+
+//     while (startColumn <= endColumn && startRow <= endRow) {
+//         // Top row
+//         for (let i = startColumn; i <= endColumn; i++) {
+//             results[startRow][i] = count;
+//             count++;
+//         }
+//         startRow++;
+//         // Right column
+//         for (let i = startRow; i <= endRow; i++) {
+//             results[i][endColumn] = count;
+//             count++;
+//         }
+//         endColumn--;
+//         // Bottom row
+//         for (let i = endColumn; i >= startColumn; i--) {
+//             results[endRow][i] = count;
+//             count++;
+//         }
+//         endRow--;
+//         // Start column
+//         for (let i = endRow; i >= startRow; i--) {
+//             results[i][startColumn] = count;
+//             count++;
+//         }
+//         startColumn++;
+//     }
+//     return results;
+// }
+
 function matrix(n) {
-    const results = [];
-
-    for (let i = 0; i < n; i++) {
-        results.push([]);
-    }
-
-    let count = 1;
-    let startColumn = 0;
-    let startRow = 0;
-    let endColumn = n - 1;
-    let endRow = n - 1;
-
-    while (startColumn <= endColumn && startRow <= endRow) {
-        // Top row
-        for (let i = startColumn; i <= endColumn; i++) {
-            results[startRow][i] = count;
-            count++;
-        }
-        startRow++;
-        // Right column
-        for (let i = startRow; i <= endRow; i++) {
-            results[i][endColumn] = count;
-            count++;
-        }
-        endColumn--;
-        // Bottom row
-        for (let i = endColumn; i >= startColumn; i--) {
-            results[endRow][i] = count;
-            count++;
-        }
-        endRow--;
-        // Start column
-        for (let i = endRow; i >= startRow; i--) {
-            results[i][startColumn] = count;
-            count++;
-        }
-        startColumn++;
-    }
-    return results;
+    
 }
 
 module.exports = matrix;

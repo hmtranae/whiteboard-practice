8c022a1c42ee77778e030ff0840c4483b203705e
Hieu Tran <hmtran.ae@gmail.com>
2019-03-14 11:05:36 -0400

    solution spiral matrix by self done

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -59,7 +59,45 @@
 // }
 
 function matrix(n) {
-    
+    const result = [];
+
+    for (let i = 0; i < n; i++) {
+        result.push([]);
+    }
+
+    let count = 1;
+    let startColumn = 0;
+    let startRow = 0;
+    let endColumn = n - 1;
+    let endRow = n - 1;
+
+    while (startColumn <= endColumn && startRow <= endRow) {
+        // Top row
+        for (let i = startColumn; i <= endColumn; i++) {
+            result[startRow][i] = count;
+            count++;
+        }        
+        startRow++;
+        // Right column
+        for (let i = startRow; i <= endRow; i++) {
+            result[i][endColumn] = count;
+            count++;
+        }
+        endColumn--;
+        // Bottom row
+        for (let i = endColumn; i >= startColumn; i--) {
+            result[endRow][i] = count;
+            count++;
+        }
+        endRow--;
+        // Start column
+        for (let i = endRow; i >= startRow; i--) {
+            result[i][startColumn] = count;
+            count++;
+        }
+        startColumn++;       
+    }
+    return result;
 }
 
 module.exports = matrix;

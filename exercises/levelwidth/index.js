dd4e7dccf8d524d03eecbed428ee9b253e35a86a
Hieu Tran <hmtran.ae@gmail.com>
2019-03-27 18:31:49 -0400

    solution to levelwidth with bfs traversal

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,25 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let array = [root, 'stop'];
+    let counterArray = [0];
+    while (array.length > 1) {
+        // if first ele is not 'first'
+        if (array[0] !== 'stop') {
+            let node = array.shift();
+            array.push(...node.children);
+            counterArray[counterArray.length - 1] ++;
+        } else {
+            // if first ele is 'first'
+            array.push(array.shift());
+            counterArray.push(0);
+        }
+    }
+    return counterArray;
+}
+
+// [1, 2, 3, 'stop']
+// [1, ]
 
 module.exports = levelWidth;

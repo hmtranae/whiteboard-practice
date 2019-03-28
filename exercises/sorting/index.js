1ec0f31129fb4033be8e30d9bd16156d07e9b42a
Hieu Tran <hmtran.ae@gmail.com>
2019-03-28 09:08:38 -0400

    merge function for mergesort implementation

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -44,7 +44,16 @@ function mergeSort(arr) {
 }
 
 function merge(left, right) {
+    let results = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift())
+        } else {
+            results.push(right.shift())
+        }
+    }
 
+    return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };

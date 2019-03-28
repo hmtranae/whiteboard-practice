b88419056d247767c115cad06390c139d692ee3e
Hieu Tran <hmtran.ae@gmail.com>
2019-03-27 20:50:45 -0400

    check node in binary search tree using recursion

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -32,7 +32,17 @@ class Node {
     }
 
     contains(data) {
+        if (this.data === data) {
+            return this;
+        }
+        
+        if (data < this.data && this.left) {
+            return this.left.contains(data);
+        } else if (data > this.data && this.right) {
+            return this.right.contains(data);
+        }
 
+        return null;
     }
 }
 

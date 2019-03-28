7ebe6f0c13ee91a5bb374a8b1695fe815c7f462d
Hieu Tran <hmtran.ae@gmail.com>
2019-03-27 21:53:36 -0400

    refactor validate bst solution

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -6,19 +6,19 @@
 // the parent
 
 function validate(node, min = null, max = null) {
-    if (!node) {
-        return true;
-    }
     if (max !== null && node.data >= max) {
         return false;
     }
     if (min !== null && node.data <= min) {
         return false;
     }
-    const left = validate(node.left, min, node.data);
-    const right = validate(node.right, node.data, max);
-
-    return left && right;
+    if (node.left && !validate(node.left, min, node.data)) {
+        return false;
+    }
+    if (node.right && !validate(node.right, node.data, max)) {
+        return false;
+    }
+    return true;
 }
 
 module.exports = validate;
